6efce5f7582a5602c14d5021808f14da759d0271
Neil Caruana <neilcaruana@gmail.com>
2026-02-06 18:43:02 +0100

    Init of Realtime dashboard

--- ReverseProxyServer.Web/Services/DashboardDataService.cs
+++ ReverseProxyServer.Web/Services/DashboardDataService.cs
@@ -1,4 +1,6 @@
+using Kavalan.Data.Sqlite;
 using Kavalan.Data.Sqlite.Repositories;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Options;
 using ReverseProxyServer.Data.DTO;
 
@@ -21,14 +23,14 @@ public class DashboardDataService : IDashboardDataService
         using var repo = new GenericSqliteRepository<Connection>(_databasePath);
         string dateFilter = GetDateFilter(fromDate, toDate);
 
+        var totalCount = await repo.CountAsync(dateFilter);
         var forwardCount = await repo.CountAsync($"{dateFilter} AND ProxyType = 'Forward'");
-        var honeypotCount = await repo.CountAsync($"{dateFilter} AND ProxyType = 'HoneyPot'");
 
         return new ConnectionStats
         {
-            TotalConnections = forwardCount + honeypotCount,
+            TotalConnections = totalCount,
             ForwardConnections = forwardCount,
-            HoneypotConnections = honeypotCount
+            HoneypotConnections = totalCount - forwardCount
         };
     }
 
@@ -54,38 +56,114 @@ public class DashboardDataService : IDashboardDataService
 
     public async Task<PagedResult<Connection>> GetConnectionsPagedAsync(DateTime fromDate, DateTime toDate, int page, int pageSize, ConnectionFilter? filter = null)
     {
-        using var repo = new GenericSqliteRepository<Connection>(_databasePath);
+        var dataLayer = new SqlLiteDataLayer(_databasePath);
+        using var connection = await dataLayer.GetOpenConnection();
+
         string whereClause = GetDateFilter(fromDate, toDate);
 
         if (filter != null)
         {
             if (!string.IsNullOrWhiteSpace(filter.ProxyType))
-                whereClause += $" AND ProxyType = '{filter.ProxyType.Replace("'", "''")}'";
+                whereClause += $" AND c.ProxyType = @ProxyType";
             if (!string.IsNullOrWhiteSpace(filter.RemoteAddress))
-                whereClause += $" AND RemoteAddress LIKE '%{filter.RemoteAddress.Replace("'", "''")}%'";
+                whereClause += $" AND c.RemoteAddress LIKE @RemoteAddress";
             if (!string.IsNullOrWhiteSpace(filter.RemotePort))
-                whereClause += $" AND RemotePort = '{filter.RemotePort.Replace("'", "''")}'";
+                whereClause += $" AND c.RemotePort = @RemotePort";
             if (!string.IsNullOrWhiteSpace(filter.LocalAddress))
-                whereClause += $" AND LocalAddress LIKE '%{filter.LocalAddress.Replace("'", "''")}%'";
+                whereClause += $" AND c.LocalAddress LIKE @LocalAddress";
             if (!string.IsNullOrWhiteSpace(filter.LocalPort))
-                whereClause += $" AND LocalPort = '{filter.LocalPort.Replace("'", "''")}'";
+                whereClause += $" AND c.LocalPort = @LocalPort";
         }
 
-        long totalCount = await repo.CountAsync(whereClause);
         int offset = (page - 1) * pageSize;
 
-        var connections = await repo.SelectDataByExpressionAsync(
-            fieldName: "",
-            fieldValue: null,
-            whereClause: whereClause,
-            orderByCaluse: $"ConnectionTime DESC LIMIT {pageSize} OFFSET {offset}");
+        string sql = $"""
+            SELECT c.Id, c.SessionId, c.InstanceId, c.ConnectionTime, c.ProxyType, 
+                   c.LocalAddress, c.LocalPort, c.TargetHost, c.TargetPort, 
+                   c.RemoteAddress, c.RemotePort,
+                   EXISTS(SELECT 1 FROM ConnectionsData cd WHERE cd.SessionId = c.SessionId) AS HasData
+            FROM Connections c
+            WHERE {whereClause}
+            ORDER BY c.ConnectionTime DESC
+            LIMIT @PageSize OFFSET @Offset
+            """;
+
+        string countSql = $"SELECT COUNT(*) FROM Connections c WHERE {whereClause}";
+
+        using var countCmd = new SqliteCommand(countSql, connection);
+        using var dataCmd = new SqliteCommand(sql, connection);
+
+        // Add shared parameters to both commands
+        countCmd.Parameters.AddWithValue("@PageSize", pageSize);
+        countCmd.Parameters.AddWithValue("@Offset", offset);
+        dataCmd.Parameters.AddWithValue("@PageSize", pageSize);
+        dataCmd.Parameters.AddWithValue("@Offset", offset);
+
+        if (filter != null)
+        {
+            if (!string.IsNullOrWhiteSpace(filter.ProxyType))
+            {
+                countCmd.Parameters.AddWithValue("@ProxyType", filter.ProxyType);
+                dataCmd.Parameters.AddWithValue("@ProxyType", filter.ProxyType);
+            }
+            if (!string.IsNullOrWhiteSpace(filter.RemoteAddress))
+            {
+                countCmd.Parameters.AddWithValue("@RemoteAddress", $"%{filter.RemoteAddress}%");
+                dataCmd.Parameters.AddWithValue("@RemoteAddress", $"%{filter.RemoteAddress}%");
+            }
+            if (!string.IsNullOrWhiteSpace(filter.RemotePort))
+            {
+                countCmd.Parameters.AddWithValue("@RemotePort", filter.RemotePort);
+                dataCmd.Parameters.AddWithValue("@RemotePort", filter.RemotePort);
+            }
+            if (!string.IsNullOrWhiteSpace(filter.LocalAddress))
+            {
+                countCmd.Parameters.AddWithValue("@LocalAddress", $"%{filter.LocalAddress}%");
+                dataCmd.Parameters.AddWithValue("@LocalAddress", $"%{filter.LocalAddress}%");
+            }
+            if (!string.IsNullOrWhiteSpace(filter.LocalPort))
+            {
+                countCmd.Parameters.AddWithValue("@LocalPort", filter.LocalPort);
+                dataCmd.Parameters.AddWithValue("@LocalPort", filter.LocalPort);
+            }
+        }
+
+        long totalCount = Convert.ToInt64(await countCmd.ExecuteScalarAsync());
+
+        var connections = new List<Connection>();
+        var sessionsWithData = new HashSet<string>();
+
+        using var reader = await dataCmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            var conn = new Connection
+            {
+                Id = reader.GetInt64(reader.GetOrdinal("Id")),
+                SessionId = reader.GetString(reader.GetOrdinal("SessionId")),
+                InstanceId = reader.GetString(reader.GetOrdinal("InstanceId")),
+                ConnectionTime = Convert.ToDateTime(reader["ConnectionTime"]),
+                ProxyType = reader.GetString(reader.GetOrdinal("ProxyType")),
+                LocalAddress = reader.GetString(reader.GetOrdinal("LocalAddress")),
+                LocalPort = reader.GetInt64(reader.GetOrdinal("LocalPort")),
+                TargetHost = reader.GetString(reader.GetOrdinal("TargetHost")),
+                TargetPort = reader.GetInt64(reader.GetOrdinal("TargetPort")),
+                RemoteAddress = reader.GetString(reader.GetOrdinal("RemoteAddress")),
+                RemotePort = reader.GetInt64(reader.GetOrdinal("RemotePort"))
+            };
+
+            connections.Add(conn);
+
+            if (reader.GetInt64(reader.GetOrdinal("HasData")) == 1)
+                sessionsWithData.Add(conn.SessionId);
+        }
 
         return new PagedResult<Connection>
         {
             Items = connections,
             TotalCount = (int)totalCount,
             Page = page,
-            PageSize = pageSize
+            PageSize = pageSize,
+            SessionsWithData = sessionsWithData
         };
     }
 
